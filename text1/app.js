45f3922478f82b921e4408b529e73c8f72af71e0
160920021 <2050569473@qq.com>
2019-08-09 13:57:22 +0800

    第一个小程序

---
+++ text1/app.js
@@ -0,0 +1,21 @@
+//app.js
+//监听生命周期函数,在加载完app.json后加装JS文件
+App({
+onLaunch(){
+  // 获取用户信息1
+//  wx.getUserInfo({
+//    success(res){
+//      console.log(res)
+//    }
+//  })
+},
+onShow(option){
+
+},
+onHide(){
+
+},
+onError(){
+
+}
+})
---
+++ text1/pages/demo1/demo1.js
@@ -0,0 +1,77 @@
+// pages/demo1/demo1.js
+Page({
+
+  /**
+   * 页面的初始数据
+   */
+  data: {
+  message:'点击我开始计数',
+  click:null
+  },
+
+  /**
+   * 生命周期函数--监听页面加载
+   */
+  onLoad: function (options) {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面初次渲染完成
+   */
+  onReady: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面显示
+   */
+  onShow: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面隐藏
+   */
+  onHide: function () {
+
+  },
+
+  /**
+   * 生命周期函数--监听页面卸载
+   */
+  onUnload: function () {
+
+  },
+
+  /**
+   * 页面相关事件处理函数--监听用户下拉动作
+   */
+  onPullDownRefresh: function () {
+
+  },
+
+  /**
+   * 页面上拉触底事件的处理函数
+   */
+  onReachBottom: function () {
+
+  },
+
+  /**
+   * 用户点击右上角分享
+   */
+  onShareAppMessage: function () {
+
+  },
+  textclick(){
+    this.setData({
+      message:'我被点击了',
+      click:this.data.click+1
+    })
+  },
+  // getUserRoot(event){
+  // console.log(event)
+  // }
+  // 获取用户信息2
+})
